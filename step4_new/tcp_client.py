5a3b5f5b74bad52fac6bfb1a94fd8704bf808a78
align-els <aligng1620@gmail.com>
2019-07-27 17:34:38 +0300

    refactoring

---
+++ step4_new/tcp_client.py
@@ -0,0 +1,35 @@
+import socket
+import hashlib
+from random import randrange
+
+
+relay_addr = ("127.0.0.1", 1236)
+fname = "f_to_send.png"
+
+
+def fhash(data):
+	hasher = hashlib.md5()
+	hasher.update(data)
+	hashed = hasher.digest()
+	return hashed
+
+def send_data(filename):
+	sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sender.connect(relay_addr)
+	f = open (filename, "rb")
+
+	while True:
+		data = f.read(1024)
+		if not data: 
+			break
+		hash = fhash(data)
+		data += hash
+		sender.send(data)
+		while sender.recv(1) != b'+':
+			sender.send(data)
+	sender.close()
+
+
+
+if __name__ == '__main__':
+	send_data(fname)
